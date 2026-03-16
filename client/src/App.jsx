4cf3cfb92ad615685a77d81d9cb409a7c61c1185
Stephen Ejiro <remote.stephen11@gmail.com>
2026-03-16 23:10:46 +0100

    Switch to HashRouter for reliable Vercel routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -23,7 +23,7 @@ import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="app-shell">
         <Navbar />
         <main className="app-main py-4 sm:py-6">
@@ -60,7 +60,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
